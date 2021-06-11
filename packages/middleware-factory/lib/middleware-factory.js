99c0c3aabba35ce351981a890893569aadf2d8fd
Tirtha Guha <tirtha.guha@gmail.com>
2021-06-11 19:54:47 +0530

    refactor(middleware-factory): added logger
    Added logger to middleware factory

--- packages/middleware-factory/lib/middleware-factory.js
+++ packages/middleware-factory/lib/middleware-factory.js
@@ -1,38 +1,38 @@
 "use strict";
-const logger = require("@rapidcode/logger");
+
+const createLogger = require("../../../common/logger");
+const logger = createLogger();
 
 const middlewareFactory = {
-  createMiddleware: ({ log = true, injectData = false, func }) => (
-    req,
-    res,
-    next
-  ) => {
-    try {
-      const response = injectData ? func(req, res) : func();
-      res.locals[`${func.name}Response`] = response;
-      next();
-    } catch (error) {
-      next({ message: error });
-    }
-  },
+  createMiddleware:
+    ({ log = true, injectData = false, func }) =>
+    (req, res, next) => {
+      try {
+        const response = injectData ? func(req, res) : func();
+        res.locals[`${func.name}Response`] = response;
+        next();
+      } catch (error) {
+        next({ message: error });
+      }
+    },
 
-  createAsyncMiddleware: ({ log = true, injectData = false, func }) => async (
-    req,
-    res,
-    next
-  ) => {
-    try {
-      const t1 = Date.now();
-      const response = injectData ? await func(req, res.locals) : await func();
-      const t2 = Date.now();
-      logger.info(`Time Taken to execute async ${func.name} is ${t2 - t1}`);
-      res.locals[`${func.name}Response`] = response;
-      logger.info("response = " + JSON.stringify(response));
-      next();
-    } catch (error) {
-      logger.error(error);
-      next({ message: error });
-    }
-  },
+  createAsyncMiddleware:
+    ({ log = true, injectData = false, func }) =>
+    async (req, res, next) => {
+      try {
+        const t1 = Date.now();
+        const response = injectData
+          ? await func(req, res.locals)
+          : await func();
+        const t2 = Date.now();
+        logger.info(`Time Taken to execute async ${func.name} is ${t2 - t1}`);
+        res.locals[`${func.name}Response`] = response;
+        logger.info("response = " + JSON.stringify(response));
+        next();
+      } catch (error) {
+        logger.error(error);
+        next({ message: error });
+      }
+    },
 };
 module.exports = middlewareFactory;
