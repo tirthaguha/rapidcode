77cc15d800b76316887ef99e83dd37cf82f6247c
Tirtha Guha <tirtha.guha@gmail.com>
2021-06-28 23:48:56 +0530

    fix(middleware): changed to accomodate the new logger and changed how wrappedFunction is called

--- packages/middleware/lib/middleware.js
+++ packages/middleware/lib/middleware.js
@@ -1,15 +1,17 @@
 "use strict";
 
-const createWinstonLogger = require("@rapidcode/logger");
-const logger = createWinstonLogger();
+const getLogger = require("@rapidcode/logger");
+const logger = getLogger("@rapidcode/middleware");
 
 const middlewareFactory = {
   createMiddleware:
-    ({ log = true, injectData = false, func }) =>
+    ({ log = true, func }) =>
     (req, res, next) => {
       try {
-        const response = injectData ? func(req, res) : func();
-        res.locals[`${func.name}Response`] = response;
+        const response = func(req, res);
+        if (response) {
+          res.locals[`${func.name}Response`] = response;
+        }
         next();
       } catch (error) {
         next({ message: error });
@@ -17,16 +19,16 @@ const middlewareFactory = {
     },
 
   createAsyncMiddleware:
-    ({ log = true, injectData = false, func }) =>
+    ({ log = true, func }) =>
     async (req, res, next) => {
       try {
         const t1 = Date.now();
-        const response = injectData
-          ? await func(req, res.locals)
-          : await func();
+        const response = await func(req, res);
         const t2 = Date.now();
         logger.info(`Time Taken to execute async ${func.name} is ${t2 - t1}`);
-        res.locals[`${func.name}Response`] = response;
+        if (response) {
+          res.locals[`${func.name}Response`] = response;
+        }
         logger.info("response = " + JSON.stringify(response));
         next();
       } catch (error) {
