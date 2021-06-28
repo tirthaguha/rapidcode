30565a2eb5957db9721e30b08d880e409fe19377
Tirtha Guha <tirtha.guha@gmail.com>
2021-06-28 23:46:57 +0530

    refactor(logger): changed the logger implementation to reuse loggers instead of creating loggers
    if a logger is already created, we'll be reusing it

--- packages/logger/lib/logger.js
+++ packages/logger/lib/logger.js
@@ -1,20 +1,48 @@
 "use strict";
-// const { info } = require("winston");
+
+let loggerRegistry = [];
+
 const { createLogger, format, transports } = require("winston");
 const { combine, timestamp, label, printf } = format;
 
 const myFormat = printf(({ level, message, label, timestamp }) => {
-  return `${timestamp} [${label}] ${level}: ${message}`;
+  return `{"timestamp":${timestamp}, "label":[${label}],${level}: "${message}"}`;
 });
 
-const createWinstonLogger = (level = "info") => {
+const randomString = (length) => {
+  const chars =
+    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var result = "";
+  for (var i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
+
+const standardConfigObj = { loggerName: randomString(8), level: "info" };
+
+const createWinstonLogger = (configObj = standardConfigObj) => {
+  const { loggerName, level } = configObj;
   const defaultLogLevel = level;
   const logger = createLogger({
     level: defaultLogLevel,
-    format: combine(label({ label: "right meow!" }), timestamp(), myFormat),
+    format: combine(label({ label: loggerName }), timestamp(), myFormat),
     transports: [new transports.Console()],
   });
+  // name = name === undefined ? randomString(8) : name;
+
+  loggerRegistry[loggerName] = logger;
   return logger;
 };
 
-module.exports = createWinstonLogger;
+const getLogger = (loggerName) => {
+  if (!loggerRegistry[loggerName]) {
+    createWinstonLogger({ loggerName: loggerName });
+  }
+  return loggerRegistry[loggerName];
+};
+
+const configLogger = (configObj) => {
+  // TODO: add option to configure the logger
+};
+
+module.exports = getLogger;
