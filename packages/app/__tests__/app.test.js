62aca43c5f5284c9280416d3e3c970f7b0702bef
Tirtha Guha <tirtha.guha@gmail.com>
2021-08-21 16:24:45 +0530

    test(app): added test for app

--- packages/app/__tests__/app.test.js
+++ packages/app/__tests__/app.test.js
@@ -12,10 +12,63 @@ describe("@rapidcode/app", () => {
   test("test route attachment", () => {
     const app = createApp();
     let testRouter = express.Router();
-    testRouter.route("/")["get"]((req, res) => {
+    let testRouter2 = express.Router();
+    testRouter.route("/get")["get"]((req, res) => {
       res.send({});
     });
-    registerMultipleRoutes(app, [{ path: "/users", router: testRouter }]);
-    expect(app).toBeDefined();
+    testRouter2.route("/post")["post"]((req, res) => {
+      res.send({});
+    });
+    registerMultipleRoutes(app, [{ path: "/users", router: testRouter }, { path: "/products", router: testRouter2 }]);
+    app.use('/test', (req, res) => {//doesn't show up
+      res.send({});
+    })
+
+    const routes = {}
+    function routerRecursion(middleware, pointer) {
+      // console.log(middleware.route);
+      if (middleware.route) { // routes registered directly on the app
+        if (!Array.isArray(pointer['routes'])) {
+          pointer['routes'] = []
+        }
+        const routeObj = {
+          path: middleware.route.path,
+          method: middleware.route.stack[0].method
+        }
+        pointer['routes'].push(routeObj)
+      } else if (middleware.name === 'router') { // inside router
+        const current = middleware.regexp.toString().replace(/\/\^\\\//, '').replace(/\\\/\?\(\?\=\\\/\|\$\)\/\i/, '')
+        pointer[current] = {}
+        middleware.handle.stack.forEach(function (handler) {
+          routerRecursion(handler, pointer[current], current)
+        });
+      }
+    }
+    app._router.stack.forEach(function (middleware) {
+      routerRecursion(middleware, routes)
+    });
+    console.log(JSON.stringify(routes,null, 2));
+
+    
+
+    // console.log(app._router.stack.filter(r=>r))
+    expect(routes).toMatchObject(  {
+      "users": {
+        "routes": [
+          {
+            "path": "/get",
+            "method": "get"
+          }
+        ]
+      },
+      "products": {
+        "routes": [
+          {
+            "path": "/post",
+            "method": "post"
+          }
+        ]
+      }
+    });
   });
 });
--- packages/app/jest.config.js
+++ packages/app/jest.config.js
@@ -9,7 +9,7 @@ const config = {
     "!**/coverage/**",
   ],
   coverageDirectory: "coverage",
-  coverageReporters: ["json", "lcov"],
+  coverageReporters: ["json", "lcov", "text"],
   coverageThreshold: {
     global: {
       branches: 90,
