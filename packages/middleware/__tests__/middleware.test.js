982eb22e8d0c04f98e34f499b5ed09ae22412cc3
Tirtha Guha <tirtha.guha@gmail.com>
2021-08-21 18:51:41 +0530

    test(middleware): added tests for sync middlewares

--- packages/middleware/__tests__/middleware.test.js
+++ packages/middleware/__tests__/middleware.test.js
@@ -1,7 +1,67 @@
 'use strict';
 
-const middleware = require('..');
+const {createMiddleware, createAsyncMiddleware} = require('../lib/middleware');
 
 describe('@rapidcode/middleware', () => {
-    it('needs tests');
+    test('should invoke middleware synchronous and execute', () => {
+        let mockObj = false;
+        const mockFn = ()=>{
+            mockObj=true;
+        }
+        const mw = createMiddleware({log:true, func:mockFn})
+        mw({},{}, ()=>{});
+        expect(mockObj).toBe(true);
+    });
+
+    test('should invoke next if successful', () => {
+        let mockObj = false;
+        const mockFn = ()=>{
+        }
+        const mw = createMiddleware({func:mockFn})
+        mw({},{}, ()=>{mockObj=true;});
+        expect(mockObj).toBe(true);
+    });
+
+    test('should attach the return of the function to res.locals', () => {
+        const mockFn = ()=>{
+            return true;
+        }
+        const mw = createMiddleware({func:mockFn})
+        const res = {locals:{}};
+        mw({},res, ()=>{});
+        // console.log(res);
+        expect(res).toEqual({locals:{
+            mockFnResponse:true
+        }});
+    });
+
+    test('should handle the error, with no status', () => {
+        const mockFn = ()=>{
+            throw Error("Random Error");
+        }
+        const mw = createMiddleware({log:false, func:mockFn});
+        let errorObj = {};
+        mw({},{}, (err)=>{ 
+            errorObj = err;
+        });
+        expect(errorObj).toEqual( {
+            status: 500,
+            message: "Random Error"
+          });
+    });
+
+    test('should handle the error with status', () => {
+        const mockFn = ()=>{
+            throw ({message:"Random Error", status: 400});
+        }
+        const mw = createMiddleware({log:false, func:mockFn});
+        let errorObj = {};
+        mw({},{}, (err)=>{ 
+            errorObj = {message:err.message, status:err.status};
+        });
+        expect(errorObj).toEqual( {
+            status: 400,
+            message: "Random Error"
+          });
+    });
 });
